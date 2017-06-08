382c2a75df3d8e96d64442a6a6f528301cab7938
marshallamey <mamey2@student.alamo.edu>
2017-06-07 22:41:57 -0500

    Create phSensor.ino
    Code to use with the ph Sensor from Atlas Scientific

---
+++ phSensor.ino
@@ -0,0 +1,68 @@
+//This code will output data to the Arduino serial monitor.
+//Type commands into the Arduino serial monitor to control the pH circuit.
+//This code was written in the Arduino 1.6.5 IDE
+//An Arduino UNO was used to test this code.
+
+
+#include <SoftwareSerial.h>                           //we have to include the SoftwareSerial library, or else we can't use it
+#define RX 2                                          //define what pin RX is going to be
+#define TX 3                                          //define what pin TX is going to be
+
+SoftwareSerial myserial(RX, TX);                      //define how the soft serial port is going to work
+
+
+String inputstring = "";                              //a string to hold incoming data from the PC
+String sensorstring = "";                             //a string to hold the data from the Atlas Scientific product
+boolean input_string_complete = false;                //have we received all the data from the PC
+boolean sensor_string_complete = false;               //have we received all the data from the Atlas Scientific product
+float pH;                                             //used to hold a floating point number that is the pH
+
+
+
+void setup() {                                        //set up the hardware
+  Serial.begin(9600);                                 //set baud rate for the hardware serial port_0 to 9600
+  myserial.begin(9600);                               //set baud rate for the software serial port to 9600
+  inputstring.reserve(10);                            //set aside some bytes for receiving data from the PC
+  sensorstring.reserve(30);                           //set aside some bytes for receiving data from Atlas Scientific product
+}
+
+
+void serialEvent() {                                  //if the hardware serial port_0 receives a char
+  inputstring = Serial.readStringUntil(13);           //read the string until we see a <CR>
+  input_string_complete = true;                       //set the flag used to tell if we have received a completed string from the PC
+}
+
+
+void loop() {                                         //here we go...
+
+  if (input_string_complete) {                        //if a string from the PC has been received in its entirety
+    myserial.print(inputstring);                      //send that string to the Atlas Scientific product
+    myserial.print('\r');                             //add a <CR> to the end of the string
+    inputstring = "";                                 //clear the string
+    input_string_complete = false;                    //reset the flag used to tell if we have received a completed string from the PC
+  }
+
+  if (myserial.available() > 0) {                     //if we see that the Atlas Scientific product has sent a character
+    char inchar = (char)myserial.read();              //get the char we just received
+    sensorstring += inchar;                           //add the char to the var called sensorstring
+    if (inchar == '\r') {                             //if the incoming character is a <CR>
+      sensor_string_complete = true;                  //set the flag
+    }
+  }
+
+
+  if (sensor_string_complete == true) {               //if a string from the Atlas Scientific product has been received in its entirety
+    Serial.println(sensorstring);                     //send that string to the PC's serial monitor
+    if (isdigit(sensorstring[0])) {                   //if the first character in the string is a digit
+      pH = sensorstring.toFloat();                    //convert the string to a floating point number so it can be evaluated by the Arduino
+      if (pH >= 7.0) {                                //if the pH is greater than or equal to 7.0
+        Serial.println("high");                       //print "high" this is demonstrating that the Arduino is evaluating the pH as a number and not as a string
+      }
+      if (pH <= 6.999) {                              //if the pH is less than or equal to 6.999
+        Serial.println("low");                        //print "low" this is demonstrating that the Arduino is evaluating the pH as a number and not as a string
+      }
+    }
+    sensorstring = "";                                //clear the string
+    sensor_string_complete = false;                   //reset the flag used to tell if we have received a completed string from the Atlas Scientific product
+  }
+}
