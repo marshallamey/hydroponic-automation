75158e190e5c3e03f1d1fddaaee59fc35d4e71a7
Marshall <mamey2@student.alamo.edu>
2017-06-14 12:59:04 -0500

    Update hydroponicAutomation.ino
    Updated sensor communication functionality

--- hydroponicAutomation.ino
+++ hydroponicAutomation.ino
@@ -1,81 +1,57 @@
 
-//This code will output data to the Arduino serial monitor.
-//Type commands into the Arduino serial monitor to control the pH circuit.
-//This code was written in the Arduino 1.6.5 IDE
-//An Arduino UNO was used to test this code.
-
+/* 
+ *  This is a program to automate and control the parameters of a hydroponic garden.
+ *  Code was designed to be run with an Arduino Mega 2560
+ *  The project was completed by research students at San Antonio College
+*/
 
 #include <SoftwareSerial.h>                           // Include the SoftwareSerial library for DO sensor
 #include "DHT.h"                                      // Include the DHT library for DHT22 sensor
-#include <Wire.h>
-#include <Adafruit_MCP23017.h>
 #include <Adafruit_RGBLCDShield.h>
             
-//#define DHTTYPE DHT11   // DHT 11                   // Uncomment the DHT type you're using
-//#define DHTTYPE DHT21   // DHT 21 (AM2301)
 #define DHTTYPE DHT22   // DHT 22  (AM2302), (AM2321)
 #define RX 11
-#define TX 12
+#define TX 10
 #define DHTPIN 4                                      // Define D/O pin for DHT sensor
-
-// These #defines make it easy to set the backlight color of LCD
-#define RED 0x1
-#define YELLOW 0x3
-#define GREEN 0x2
-#define TEAL 0x6  
-#define BLUE 0x4
-#define VIOLET 0x5
-#define WHITE 0x7
 
 DHT dht(DHTPIN, DHTTYPE);                             // Define how the DHT sensor is going to work
 SoftwareSerial Serial4 (RX, TX);                      // Define how the soft serial port is going to work
 Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield();
 
-String inputstringRTD = "";                           // Creates a string to hold incoming data from the PC
-String sensorstringRTD = "";                          // Creates a string to hold the data from the Atlas Scientific product
-boolean input_string_RTD_complete = false;            // have we received all the data from the PC
-boolean sensor_string_RTD_complete = false;           // have we received all the data from the Atlas Scientific product
-float waterTemp;                                      // Creates a floating point number that is the pH
+boolean input_string_1_complete = false;              // have we received all the data from the PC
+boolean input_string_2_complete = false;              // have we received all the data from the PC
 
-String inputstringEC = "";                            // Creates a string to hold incoming data from the PC
-String sensorstringEC = "";                           // Creates a string to hold the data from the Atlas Scientific product
-boolean input_string_EC_complete = false;             // have we received all the data from the PC
-boolean sensor_string_EC_complete = false;            // have we received all the data from the Atlas Scientific product
-float conductivity;                                   // Creates a floating point number that is the pH
+String inputString1 = "";                             // Creates a string to hold incoming data from the PC
+String inputString2 = "";                             // Creates a string to hold incoming data from the PC
+String sensorstringRTD = "";                          // Creates a string to hold the data from the Atlas Scientific temperature sensor
+String sensorstringEC = "";                           // Creates a string to hold the data from the Atlas Scientific conductivity sensor
+String sensorstringPH = "";                           // Creates a string to hold the data from the Atlas Scientific pH sensor
+String sensorstringDO = "";                           // Creates a string to hold the data from the Atlas Scientific dissolved oxygen sensor
 
-String inputstringPH = "";                            // Creates a string to hold incoming data from the PC
-String sensorstringPH = "";                           // Creates a string to hold the data from the Atlas Scientific product
-boolean input_string_PH_complete = false;             // have we received all the data from the PC
-boolean sensor_string_PH_complete = false;            // have we received all the data from the Atlas Scientific product
+float waterTemp;                                      // Creates a floating point number that is the water temperature
+float conductivity;                                   // Creates a floating point number that is the nutrient conductivity
 float pH;                                             // Creates a floating point number that is the pH
+float oxygen;                                         // Creates a floating point number that is the dissolved oxygen
+float humidity;                                       // Creates a floating point number that is the humidity
+float airTempC;                                       // Creates a floating point number that is the air temperature (Celsius)
+float airTempF;                                       // Creates a floating point number that is the air temperature (Fahrenheit)
 
-String inputstringDO = "";                            // Creates a string to hold incoming data from the PC
-String sensorstringDO = "";                           // Creates a string to hold the data from the Atlas Scientific product
-boolean input_string_DO_complete = false;             // have we received all the data from the PC
-boolean sensor_string_DO_complete = false;            // have we received all the data from the Atlas Scientific product
-float oxygen;                                         // Creates a floating point number that is the pH
+int seconds = 5000;                                   // LCD scroll delay in milliseconds (x1000)
 
-float humidity;
-float airTempC;
-float airTempF;
 
-int seconds = 5000;                                    // LCD scroll delay in milliseconds (x1000)
-
-void setup() {                                        // Set up the hardware
+void setup() {                                        // *****SETUP FUNCTION***** //
   Serial.begin(9600);                                 // Set baud rate for the hardware serial port_0 to 9600
   Serial1.begin(9600);                                // Set baud rate for the hardware serial port_1 to 9600
   Serial2.begin(9600);                                // set baud rate for software serial port_2 to 9600
   Serial3.begin(9600);                                // set baud rate for software serial port_3 to 9600
   Serial4.begin(9600);                                // set baud rate for software serial port_4 to 9600
-  inputstringRTD.reserve(10);                         // Set aside some bytes for receiving data from the PC
+  inputString1.reserve(10);                           // Set aside some bytes for receiving data from the PC
+  inputString2.reserve(10);                           // Set aside some bytes for receiving data from the PC
   sensorstringRTD.reserve(30);                        // Set aside some bytes for receiving data from Atlas Scientific product
-  inputstringEC.reserve(10);                          // Set aside some bytes for receiving data from the PC
   sensorstringEC.reserve(30);                         // Set aside some bytes for receiving data from Atlas Scientific product
-  inputstringPH.reserve(10);                          // Set aside some bytes for receiving data from the PC
   sensorstringPH.reserve(30);                         // Set aside some bytes for receiving data from Atlas Scientific product
-  inputstringDO.reserve(10);                          // Set aside some bytes for receiving data from the PC
   sensorstringDO.reserve(30);                         // Set aside some bytes for receiving data from Atlas Scientific product
-  
+
   Serial.println("Collecting Data...");               // Display start of data collection
   dht.begin();                                        // Start DHT sensor
   lcd.begin(16, 2);                                   // Start LCD screen
@@ -83,32 +59,58 @@ void setup() {                                        // Set up the hardware
   uint8_t i=0; 
 }
 
-
 void loop() {                                         // *****LOOP FUNCTION***** //  
-  if (input_string_PH_complete == true) {             // FOR SENDING COMMANDS TO ATLAS SENSOR
-    Serial3.print(inputstringPH);                     // If a string from the PC has been received in its entirety, send that string to the Atlas Scientific product
-    Serial3.print('\r');                              // Add a <CR> to the end of the string
-    inputstringPH = "";                               // Clear the string
-    input_string_PH_complete = false;                 // Reset the flag used to tell if we have received a completed string from the PC
-  }
+  sendData ();      
   readData ();
   printData ();
 }
 
-//void serialEvent() {                                  // FOR TYPING COMMANDS IN SERIAL MONITOR
-//  inputstringRTD = Serial.readStringUntil(13);           // If the hardware serial port_0 receives a char, read the string until we see a <CR>
-//  input_string_RTD_complete = true;                       // Set the flag used to tell if we have received a completed string from the PC
-//}
+void serialEvent() {                                  // *****READING USER COMMANDS IN SERIAL MONITOR***** //  
+  inputString1 = Serial.readStringUntil(13);          // If the hardware serial port_0 receives a char, read the string until we see a <CR>
+  input_string_1_complete = true; 
+  inputString2 = Serial.readStringUntil(13);          // If the hardware serial port_0 receives a char, read the string until we see a <CR>
+  input_string_2_complete = true;
+ 
+  Serial.println(inputString1);
+  Serial.println(inputString2);                       
+}
+
+void sendData () {                                    // *****SENDING COMMANDS TO ATLAS SENSORS***** //
+  
+  if (input_string_1_complete == true && input_string_2_complete == true) { 
+      
+    if (inputString1 == "RTD") {
+      Serial1.print(inputString2);                    // If a string from the PC has been received in its entirety, send that string to the Atlas Scientific product
+      Serial1.print('\r'); 
+    }
+    else if (inputString1 == "EC") {
+      Serial2.print(inputString2);                    // If a string from the PC has been received in its entirety, send that string to the Atlas Scientific product
+      Serial2.print('\r'); 
+    }
+    else if (inputString1 == "PH") {
+      Serial3.print(inputString2);                    // If a string from the PC has been received in its entirety, send that string to the Atlas Scientific product
+      Serial3.print('\r');                            // Add a <CR> to the end of the string
+    }
+    else if (inputString1 == "DO") {
+      Serial4.print(inputString2);                    // If a string from the PC has been received in its entirety, send that string to the Atlas Scientific product
+      Serial4.print('\r'); 
+    }
+  inputString1 = "";                                  // Clear the string1
+  inputString2 = "";                                  // Clear the string2
+  input_string_1_complete = false;                    // Reset the flag used to tell if we have received a completed string from the PC
+  input_string_2_complete = false;                    // Reset the flag used to tell if we have received a completed string from the PC
+  }
+}
 
 void readData() {                                     // *****FUNCTION TO READ DATA FROM SENSORS***** //  
-  sensorstringRTD = Serial1.readStringUntil(13);      //read the string until we see a <CR>
+  sensorstringRTD = Serial1.readStringUntil(13);      // Read the string until we see a <CR>
   waterTemp = sensorstringRTD.toFloat();
   waterTemp = waterTemp*1.8+32;                       // Convert from Celsius to Fahrenheit
         
-  sensorstringEC = Serial2.readStringUntil(13);       //read the string until we see a <CR>
+  sensorstringEC = Serial2.readStringUntil(13);       // Read the string until we see a <CR>
   conductivity = sensorstringEC.toFloat();            // Convert the string to a floating point number so it can be evaluated by the Arduino                            
         
-  sensorstringPH = Serial3.readStringUntil(13);       //read the string until we see a <CR>
+  sensorstringPH = Serial3.readStringUntil(13);       // Read the string until we see a <CR>
   pH = sensorstringPH.toFloat();                      // Convert the string to a floating point number so it can be evaluated by the Arduino
       
   if (Serial4.available() > 0) {                      // If we see that the Atlas Scientific DO has sent a character
@@ -218,19 +220,15 @@ void printData () {                                   // *****FUNCTION TO PRINT 
     
     if (buttons & BUTTON_DOWN) {                      // Down button instructions
       lcd.print("DOWN ");
-      lcd.setBacklight(YELLOW);
     }
     if (buttons & BUTTON_LEFT) {                      // Left button instructions
       lcd.print("LEFT ");
-      lcd.setBacklight(GREEN);
     }
     if (buttons & BUTTON_RIGHT) {                     // Right button instructions
       lcd.print("RIGHT ");
-      lcd.setBacklight(TEAL);
     }
     if (buttons & BUTTON_SELECT) {                    // Select button instructions
       lcd.print("SELECT ");
-      lcd.setBacklight(VIOLET);
     }
   }  
 }
