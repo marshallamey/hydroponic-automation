a63fb1564514b96109114bf1f76ce74b8a707d35
Marshall <mamey2@student.alamo.edu>
2017-07-31 17:29:46 -0500

    Update and rename photon.ino to photonToThingSpeak.ino
    Complete working sketch with reliable data transmission

--- photonToThingSpeak.ino
+++ photonToThingSpeak.ino
@@ -1,21 +1,27 @@
-#include "ThingSpeak.h"
+/*******************************************************************************************
+This program is meant to be used with hydroponicAutomation.ino which can be found at
+https://github.com/marshallamey/hydroponicAutomation
+*******************************************************************************************/
 
-TCPClient client;
-
-/*
-  *****************************************************************************************
+/******************************************************************************************
   **** Visit https://www.thingspeak.com to sign up for a free account and create
   **** a channel.  The video tutorial http://community.thingspeak.com/tutorials/thingspeak-channels/
   **** has more information. You need to change this to your channel, and your write API key
   **** IF YOU SHARE YOUR CODE WITH OTHERS, MAKE SURE YOU REMOVE YOUR WRITE API KEY!!
   *****************************************************************************************/
+
+#include <ThingSpeak.h>
+
+TCPClient client;
+
 // Thinkspeak channel information
 char thingSpeakAddress[] = "api.thingspeak.com";
 unsigned long myChannelNumber = 298095;
 String myWriteAPIKey = "F8Q6F3XFDI3QDLUY";
 
-//Prints whatever comes in over serial
-void setup() {
+
+void setup() 
+{
     ThingSpeak.begin(client);
     Serial.begin(9600);
     Serial1.begin(9600);
@@ -24,13 +30,17 @@ void setup() {
 
 void loop()
 {
-    String data="I am null";
+    String data = "This string is empty!";
+    
 	if(Serial1.available())
 	{
 	    data = Serial1.readStringUntil(13);
-
-	    Serial.print(data);
-        ThingSpeakUpdate(data);
+        if (data.length() > 1)
+        {
+            Serial.println("Data received!");
+            ThingSpeakUpdate(data);
+        }
+        else { Serial.println("No data received!"); }
     }
 }
 
@@ -43,14 +53,16 @@ Returns:
 void ThingSpeakUpdate(String readings)
 
 {
-    Serial.println("Date string: " + readings);
+    Serial.println("Data string: " + readings);
+    Serial.print("Length of the string: ");
+    Serial.println(readings.length());
 
-    Serial.println("...Connecting to Thingspeak");
+    Serial.println("Connecting to Thingspeak...");
 
     // Connecting and sending data to Thingspeak
     if(client.connect(thingSpeakAddress, 80))
     {
-        Serial.println("...Connection succesful, updating datastreams");
+        Serial.println("Connection succesful, updating ThingSpeak...");
 
         client.print("POST /update HTTP/1.1\n");
         client.print("Host: api.thingspeak.com\n");
@@ -67,10 +79,14 @@ void ThingSpeakUpdate(String readings)
         delay(200);
 
         Serial.println("Thingspeak update sent.");
+        Serial.println();
     }
-    else{
+    
+    else
+    {
         // Failed to connect to Thingspeak
         Serial.println("Unable to connect to Thingspeak.");
+        Serial.println();
     }
 
     if(!client.connected()){
